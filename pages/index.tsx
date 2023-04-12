cae026d312b910d4925d7ae9f5df61a3390bf2ed
Shogo Kitada <shogo@owlan031114003.sras.sic.shibaura-it.ac.jp>
2023-04-12 18:55:39 +0900

    AppBar と BottomNavigation を追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,46 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { BottomNavigation,  BottomNavigationAction} from '@mui/material'
+// import Image from 'next/image'
+// import { Inter } from 'next/font/google'
+// import { BottomNavigation,  BottomNavigationAction} from '@mui/material'
+// import RestoreIcon from '@mui/icons-material/Restore';
 
-const inter = Inter({ subsets: ['latin'] })
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import ArchiveIcon from '@mui/icons-material/Archive'
+import Paper from '@mui/material/Paper';
+import DrawerAppBar from '@/components/DrawerAppBar';
+
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+
+  const [value, setValue] = React.useState(0);
+
   return (
     <main className="">
-      <h1> Hello! </h1>
 
-      <BottomNavigation
+      <DrawerAppBar />
+
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Archive" icon={<ArchiveIcon />} />
+        </BottomNavigation>
+      </Paper>
+
+      {/* <BottomNavigation
         showLabels
         value={value}
         onChange={(event, newValue) => {
@@ -19,7 +50,7 @@ export default function Home() {
         <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
         <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
         <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-      </BottomNavigation>
+      </BottomNavigation> */}
       
     </main>
   )
